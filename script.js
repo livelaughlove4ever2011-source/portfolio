7bfb2a5c7edb2b20af5caf7188037ea17f1ffd6c
livelaughlove4ever2011-source <livelaughlove4ever2011@gmail.com>
2025-11-08 12:01:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Project 1</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Project 1</h1>
+  <p>This is my first project! 🎉</p>
+  <a href="index.html">Back to portfolio</a>
+</body>
+</html>
